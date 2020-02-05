29da076205dfe9a1b79c73fa3e7fafd2c96bc317
程梦月 <1127397156@qq.com>
2020-02-05 21:00:14 +0800

    first commit ssm-crud

---
+++ ssm-crud/src/main/java/com/cc/crud/dao/DepartmentMapper.java
@@ -0,0 +1,30 @@
+package com.cc.crud.dao;
+
+import com.cc.crud.bean.Department;
+import com.cc.crud.bean.DepartmentExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface DepartmentMapper {
+    long countByExample(DepartmentExample example);
+
+    int deleteByExample(DepartmentExample example);
+
+    int deleteByPrimaryKey(Integer deptId);
+
+    int insert(Department record);
+
+    int insertSelective(Department record);
+
+    List<Department> selectByExample(DepartmentExample example);
+
+    Department selectByPrimaryKey(Integer deptId);
+
+    int updateByExampleSelective(@Param("record") Department record, @Param("example") DepartmentExample example);
+
+    int updateByExample(@Param("record") Department record, @Param("example") DepartmentExample example);
+
+    int updateByPrimaryKeySelective(Department record);
+
+    int updateByPrimaryKey(Department record);
+}
